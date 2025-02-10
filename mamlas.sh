5d57543635a141030747af1453207e9a1cfbfe91
simon <simon.janousekk@gmail.com>
2025-02-10 19:21:50 +0100

    yipe

--- mamlas.sh
+++ mamlas.sh
@@ -1,22 +1,27 @@
-processing="$HOME/Downloads/processing-4.3.2/processing-java"
-mamlaspath="$HOME/Documents/mamlas1"
+processing="/home/ddt/Downloads/processing-4.3.2/processing-java"
+mamlaspath="/home/ddt/Documents/mamlas1"
 
 function mamlas ()
 {
     local cmd="$1"
+    local currentpath=$(pwd)
 
     case "$cmd" in
         "dev")
-            "$processing" --sketch="$mamlaspath/processing" --run
+            $processing --sketch=$mamlaspath/processing --run
             ;;
 	"run")
 	    "$mamlaspath/build/processing"
 	    ;;
         "build")
-            "$processing" --sketch="$mamlaspath/processing" --output="$mamlaspath/build" --variant=linux-aarch64 --force
+            $processing --sketch=$mamlaspath/processing --output=$mamlaspath/build --variant=linux-aarch64 --force --export
             ;;
-        "")
-            cd "$mamlaspath"
+	"pull")
+	    cd $mamlaspath && git pull
+	    cd $currentpath
+	    ;;
+	"")
+            cd $mamlaspath
             ;;
         *)
             echo "Unknown command: $cmd"
