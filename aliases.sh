1d774299f030e1b8de9cc759c47bc0fdf8830727
simon <simon.janousekk@gmail.com>
2025-02-10 15:30:42 +0100

    im so smart

---
+++ aliases.sh
@@ -0,0 +1,8 @@
+mamlasprocessing="/home/ddt/Documents/mamlas1/processing"
+mamlasbuild="/home/ddt/Documents/mamlas1/build"
+
+alias updatebash="source ~/.bashrc"
+alias processing="~/Downloads/processing-4.3.2/processing-java"
+alias mamlas="cd ~/Documents/mamlas1"
+alias mamlasrun="processing --sketch=$mamlasprocessing --run"
+alias mamlasbuild="processing --sketch=$mamlasprocessing --output=$mamlasbuild --variant=linux-aarch64 --export"
